fcc08187ddbf082e49e38acfccef1d3ddac64501
DeannaCarina <deannacarina@hotmail.com>
2022-11-19 15:19:36 +0000

    Lesson 1: Learning the PHP tag

---
+++ firstPHP/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Beginnings</title>
+</head>
+<body>
+    <h1>
+        <?php 
+            echo "Hello world!";
+        ?>
+    </h1>
+    <div>
+        <p>
+            <?php 
+                echo "This is a paragraph inside of a div!";
+            ?>
+        </p>
+        <p>
+            <?php 
+                echo "This is me learning PHP, hoping not to royally mess it up. I have experience with HTML and CSS, but this is new territory!";
+            ?>
+        </p>
+    </div>
+    
+    
+</body>
+</html>
